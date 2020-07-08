79e15a45ea33a6e64211ff934d0fb5237ad9ce0b
Crowdbotics <team@crowdbotics.com>
2020-07-08 13:12:41 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/friday_nights_18717/MainActivity.java
@@ -0,0 +1,15 @@
+package com.friday_nights_18717;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "friday_nights_18717";
+    }
+}
